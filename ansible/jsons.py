9647f75ad688689e86d089ad7ee23e4b411004db
Alexey Kovtunovich <kovtalex@gmail.com>
2019-10-16 10:56:58 +0300

    fixed

--- ansible/jsons.py
+++ ansible/jsons.py
@@ -2,10 +2,14 @@ import json
 import sys
 
 if __name__ == '__main__':
-	f = json.loads(open("terraform.status").read())
-	app = f["outputs"]["app_external_ip"]["value"]
-	db = f["outputs"]["db_external_ip"]["value"]
-	out = {'app': {'hosts': [str(app)]}, 
-		'db': {'hosts': [str(db)]}}
-
-sys.stdout.write(json.dumps(out))
+  out_str = ""
+  try:
+    f = json.loads(open("terraform.status").read())
+    app = f["outputs"]["app_external_ip"]["value"]
+    db = f["outputs"]["db_external_ip"]["value"]
+    out = {'app': {'hosts': [str(app)]},'db': {'hosts': [str(db)]}}
+    out_str = json.dumps(out)
+  except:
+    pass
+  
+  sys.stdout.write(out_str)
